f6ff49826becd730e6fc6e2e8944908ee3656836
Napat <napatotaku@gmail.com>
2022-06-22 09:56:19 +0700

    new commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Logo from './components/Logo';
+import styles from './components/styles';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={[styles.title, styles.warning]}>Hello React Native</Text>
+      <Logo/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1, 
+      justifyContent:'center', 
+      alignContent:'center'
+    }, 
+    title:{
+      fontSize:20
+    }, 
+    warning:{
+      color: 'red'
+    }
+  })
+  
+export default styles;
