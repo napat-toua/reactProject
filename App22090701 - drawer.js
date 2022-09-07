1bf79fbc39c584f274434cbb335ca6c4fb402b69
Napat Touangam <107970833+napat-toua@users.noreply.github.com>
2022-09-07 10:44:50 +0700

    IMG navbar

--- App22090701 - drawer.js
+++ App22090701 - drawer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
@@ -6,9 +6,7 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem
-} from '@react-navigation/drawer';
-import HomeScreen from './screens/HomeScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
+} from '@react-navigation/drawer'
 
 const MyTheme = {
   ...DefaultTheme,
@@ -44,20 +42,17 @@ function Notification() {
 
 function CustomDrawerContent(props) {
   return (
-    <SafeAreaView>
-      <Image
-        style={styles.sideMenuProfileIcon}
-        source={require('./assets/react_logo.png')}
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label='Close Drawer'
+        onPress={() => props.navigation.closeDrawer()}
       />
-
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem
-          label='Close Drawer'
-          onPress={() => props.navigation.closeDrawer()}
-        />
-      </DrawerContentScrollView>
-    </SafeAreaView>
+      <DrawerItem
+        label='Toggle Drawer'
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+    </DrawerContentScrollView>
   )
 }
 
@@ -69,7 +64,7 @@ function MyDrawer() {
       useLegacyImplementation
       drawerContent={(props) => <CustomDrawerContent{...props} />}
     >
-      <Drawer.Screen name='Home' component={HomeScreen} />
+      <Drawer.Screen name='Feed' component={FeedScreen} />
       <Drawer.Screen name='Notification' component={Notification} />
     </Drawer.Navigator>
   )
@@ -84,14 +79,3 @@ const App = () => {
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  sideMenuProfileIcon: {
-    resizeMode: 'center',
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
-    alignSelf: 'center',
-    margin: 10
-  },
-})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,39 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import { Ionicons } from "@expo/vector-icons";
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = (props) => (
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
 
 const HomeScreen = ({ navigation }) => {
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item title="register" iconName="person-add" onPress={() => alert('register')} />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Ionicons name='home' size={30} color='#008b8b' />
       <Text>Home Screen</Text>
       <Button
-        title='เกี่ยวกับเรา'
-        onPress={() => navigation.navigate('About', {
-          email: 'reactnative.tni.ac.th'
-        })}
+        title='OPEN DRAWER'
+        onPress={() => navigation.openDrawer()}
       />
     </View>
   )
